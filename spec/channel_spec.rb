bcd24fc1f1c89e58b6d67e47e5785650ae753c80
Ilya Grigorik <ilya@igvita.com>
2010-11-06 14:37:20 -0400

    serializable

--- spec/channel_spec.rb
+++ spec/channel_spec.rb
@@ -1,4 +1,6 @@
 require 'rspec'
+require 'yaml'
+
 require 'lib/go'
 
 describe Go::Channel do
@@ -18,6 +20,11 @@ describe Go::Channel do
     c.closed?.should be_true
   end
 
+  it "should be a first class, serializable value" do
+    lambda { Marshal.dump(c) }.should_not raise_error
+    lambda { Marshal.load(Marshal.dump(c)).is_a? Channel }.should_not raise_error
+  end
+
   context "direction" do
     # A channel provides a mechanism for two concurrently executing functions to
     # synchronize execution and communicate by passing a value of a specified element
@@ -35,30 +42,20 @@ describe Go::Channel do
     end
 
     it "should support receive only" do
-        c = Channel.new(:direction => :receive, :type => String)
+      c = Channel.new(:direction => :receive, :type => String)
 
-        lambda { c << "hello"   }.should raise_error Channel::InvalidDirection
-        lambda { c.push "hello" }.should raise_error Channel::InvalidDirection
-        lambda { c.send "hello" }.should raise_error Channel::InvalidDirection
+      lambda { c << "hello"   }.should raise_error Channel::InvalidDirection
+      lambda { c.push "hello" }.should raise_error Channel::InvalidDirection
+      lambda { c.send "hello" }.should raise_error Channel::InvalidDirection
 
-        lambda { c.pop }.should_not raise_error
-        lambda { c.receive }.should_not raise_error
+      lambda { c.pop }.should_not raise_error
+      lambda { c.receive }.should_not raise_error
     end
 
     it "should default to bi-directional communication" do
       lambda { c.send "hello" }.should_not raise_error
       lambda { c.receive }.should_not raise_error
     end
-  end
-
-  context "capacity" do
-    # The capacity, in number of elements, sets the size of the buffer in the channel.
-    # If the capacity is greater than zero, the channel is asynchronous: provided the
-    # buffer is not full, sends can succeed without blocking. If the capacity is zero
-    # or absent, the communication succeeds only when both a sender and receiver are ready.
-
-    it "should default to synchronous communication"
-    it "should support asynchronous communication with buffered capacity"
   end
 
   context "typed" do
@@ -73,14 +70,25 @@ describe Go::Channel do
     end
   end
 
-  context "channels of channels" do
-    # One of the most important properties of Go is that a channel is a first-class
-    # value that can be allocated and passed around like any other. A common use of
-    # this property is to implement safe, parallel demultiplexing.
-    # - http://golang.org/doc/effective_go.html#chan_of_chan
+  context "transport" do
+    context "channels of channels" do
+      # One of the most important properties of Go is that a channel is a first-class
+      # value that can be allocated and passed around like any other. A common use of
+      # this property is to implement safe, parallel demultiplexing.
+      # - http://golang.org/doc/effective_go.html#chan_of_chan
 
-    it "should be a first class, serializable value"
-    it "should be able to pass as a value on a different channel"
+      it "should be able to pass as a value on a different channel"
+    end
+
+    context "capacity" do
+      # The capacity, in number of elements, sets the size of the buffer in the channel.
+      # If the capacity is greater than zero, the channel is asynchronous: provided the
+      # buffer is not full, sends can succeed without blocking. If the capacity is zero
+      # or absent, the communication succeeds only when both a sender and receiver are ready.
+
+      it "should default to synchronous communication"
+      it "should support asynchronous communication with buffered capacity"
+    end
   end
 
 end
