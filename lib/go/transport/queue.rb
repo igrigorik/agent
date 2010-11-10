3ede35204b552d7098f706c3d0e693aa89e5c813
Ilya Grigorik <ilya@igvita.com>
2010-11-10 08:38:26 -0500

    refactor reader/writer locks

--- lib/go/transport/queue.rb
+++ lib/go/transport/queue.rb
@@ -14,37 +14,33 @@ module Go
         if !@@registry[@name]
           @@registry[@name] = {
             :que => [],
-            :reader_wait => [],
-            :writer_wait => [],
-            :reader_mutex => Mutex.new,
-            :writer_mutex => Mutex.new
+            :wait => [],
+            :mutex => Mutex.new
           }
         end
       end
 
       def data;         @@registry[@name];  end
       def que;          data[:que]; end
-      def reader_wait;  data[:reader_wait]; end
-      def writer_wait;  data[:writer_wait]; end
-      def reader_mutex; data[:reader_mutex]; end
-      def writer_mutex; data[:writer_mutex]; end
+      def wait;  data[:wait]; end
+      def mutex; data[:mutex]; end
 
       def max; @max; end
       def size; que.size; end
       def length; que.size; end
 
       def push(obj)
-        writer_mutex.synchronize {
+        mutex.synchronize {
           while true
             break if que.length < @max
-            writer_wait.push Thread.current
-            writer_mutex.sleep
+            wait.push Thread.current
+            mutex.sleep
           end
 
           que.push obj
 
           begin
-            t = reader_wait.shift
+            t = wait.shift
             t.wakeup if t
           rescue ThreadError
             retry
@@ -55,17 +51,17 @@ module Go
       alias enq push
 
       def pop(*args)
-        reader_mutex.synchronize {
+        mutex.synchronize {
           while true
             break if !que.empty?
-            reader_wait.push Thread.current
-            reader_mutex.sleep
+            wait.push Thread.current
+            mutex.sleep
           end
 
           retval = que.shift
 
           begin
-            t = writer_wait.shift
+            t = wait.shift
             t.wakeup if t
           rescue ThreadError
             retry
