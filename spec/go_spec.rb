fcf76aa84332984a776a3a3c874414ae565f88aa
Nathan Sutton <nate@zencoder.com>
2012-03-18 23:35:53 -0700

    Spec out Agent.go!

---
+++ spec/go_spec.rb
@@ -0,0 +1,17 @@
+require "spec_helper"
+
+describe Agent do
+  it "should launch a 'goroutine' that is actually a thread" do
+    Agent.go!{}.should be_a(Thread)
+  end
+
+  it "should pass into the thread any arguments passed to it" do
+    b = nil
+    Agent.go!(1){|a| b = a }.join
+    b.should == 1
+  end
+
+  it "should raise an error if no block is passed" do
+    lambda{ Agent.go! }.should raise_error(Agent::BlockMissing)
+  end
+end
