7bd6d547992d068a17c1ac181baff46638f33a32
Nathan Sutton <nate@zencoder.com>
2012-03-21 02:31:15 -0700

    Add benchmark results

--- benchmark/sieve.rb
+++ benchmark/sieve.rb
@@ -3,8 +3,10 @@ project_lib_path = File.expand_path(File.join(File.dirname(__FILE__), "..", "lib
 $LOAD_PATH.unshift(project_lib_path)
 require 'agent'
 
+$size = (ARGV.pop || 0).to_i
+
 def generate(channels)
-  ch = channel!(:type => Integer)
+  ch = channel!(:type => Integer, :size => $size)
   channels << ch
   go!{ i = 1; loop { ch << i+= 1} }
 
@@ -12,7 +14,7 @@ def generate(channels)
 end
 
 def filter(in_channel, prime, channels)
-  out = channel!(:type => Integer)
+  out = channel!(:type => Integer, :size => $size)
   channels << out
 
   go! do
@@ -26,7 +28,7 @@ def filter(in_channel, prime, channels)
 end
 
 def sieve(channels)
-  out = channel!(:type => Integer)
+  out = channel!(:type => Integer, :size => $size)
   channels << out
 
   go! do
@@ -48,9 +50,9 @@ nth_prime = 150
 concurrency = 5
 channels = []
 
-puts "#{nth_prime}'s prime, #{concurrency} goroutines"
+puts "#{nth_prime}'s prime, #{concurrency} goroutines, channel buffer size is #{$size}"
 
-Benchmark.bm do |x|
+Benchmark.bm(7) do |x|
   x.report("receive") do
     runners = []
 
@@ -65,10 +67,110 @@ Benchmark.bm do |x|
   end
 end
 
+puts
+
 channels.each(&:close)
 
-#       (osx lion's) ruby 1.8.7-p249 - 79.6s (omg)
-#                    ruby 1.9.2-p318 - 10.1s
-#                    ruby 1.9.3-p125 - 11.4s
-# (1.8.7-p357 w/o --1.9) jruby 1.6.7 - 16.4s
-#   (1.8.7-p357 w --1.9) jruby 1.6.7 - 13.4s
+__END__
+
+The setup:
+  13" Macbook Air
+  OSX Lion 10.7.3
+  1.8 GHz Intel Core i7
+  4 GB 1333 MHz DDR3
+  SSD
+
+The command (except w/ ruby --1.9 for jruby):
+  for i in {0..3};do ruby benchmark/sieve.rb $i; done
+
+The results:
+
+1. ruby 1.8.7 (2010-01-10 patchlevel 249) [universal-darwin11.0] (OSX Lion's ruby)
+
+150's prime, 5 goroutines, channel buffer size is 0
+             user     system      total        real
+receive 28.540000   0.300000  28.840000 ( 28.758503)
+
+150's prime, 5 goroutines, channel buffer size is 1
+             user     system      total        real
+receive 26.630000   0.300000  26.930000 ( 26.868036)
+
+150's prime, 5 goroutines, channel buffer size is 2
+             user     system      total        real
+receive 26.720000   0.300000  27.020000 ( 26.945404)
+
+150's prime, 5 goroutines, channel buffer size is 3
+             user     system      total        real
+receive 26.490000   0.280000  26.770000 ( 26.711857)
+
+2. ruby 1.9.2p318 (2012-02-14 revision 34678) [x86_64-darwin11.3.0]
+
+150's prime, 5 goroutines, channel buffer size is 0
+             user     system      total        real
+receive  6.860000   2.430000   9.290000 (  8.769513)
+
+150's prime, 5 goroutines, channel buffer size is 1
+             user     system      total        real
+receive  6.720000   2.130000   8.850000 (  8.397376)
+
+150's prime, 5 goroutines, channel buffer size is 2
+             user     system      total        real
+receive  6.870000   2.050000   8.920000 (  8.502869)
+
+150's prime, 5 goroutines, channel buffer size is 3
+             user     system      total        real
+receive  6.890000   1.990000   8.880000 (  8.479757)
+
+3. ruby 1.9.3p125 (2012-02-16 revision 34643) [x86_64-darwin11.3.0]
+
+150's prime, 5 goroutines, channel buffer size is 0
+              user     system      total        real
+receive   8.120000   5.380000  13.500000 ( 10.565193)
+
+150's prime, 5 goroutines, channel buffer size is 1
+              user     system      total        real
+receive   7.810000   4.230000  12.040000 (  9.717816)
+
+150's prime, 5 goroutines, channel buffer size is 2
+              user     system      total        real
+receive   7.590000   3.620000  11.210000 (  9.244201)
+
+150's prime, 5 goroutines, channel buffer size is 3
+              user     system      total        real
+receive   8.050000   3.510000  11.560000 (  9.661748)
+
+4. jruby 1.6.7 (ruby-1.8.7-p357) (2012-02-22 3e82bc8) (Java HotSpot(TM) 64-Bit Server VM 1.6.0_29) [darwin-x86_64-java]
+
+150's prime, 5 goroutines, channel buffer size is 0
+             user     system      total        real
+receive 10.538000   0.000000  10.538000 ( 10.508000)
+
+150's prime, 5 goroutines, channel buffer size is 1
+             user     system      total        real
+receive  9.901000   0.000000   9.901000 (  9.866000)
+
+150's prime, 5 goroutines, channel buffer size is 2
+             user     system      total        real
+receive  9.468000   0.000000   9.468000 (  9.434000)
+
+150's prime, 5 goroutines, channel buffer size is 3
+             user     system      total        real
+receive 11.278000   0.000000  11.278000 ( 11.242000)
+
+5. jruby 1.6.7 (ruby-1.9.2-p312) (2012-02-22 3e82bc8) (Java HotSpot(TM) 64-Bit Server VM 1.6.0_29) [darwin-x86_64-java]
+
+150's prime, 5 goroutines, channel buffer size is 0
+             user     system      total        real
+receive 10.986000   0.000000  10.986000 ( 10.986000)
+
+150's prime, 5 goroutines, channel buffer size is 1
+             user     system      total        real
+receive 10.965000   0.000000  10.965000 ( 10.965000)
+
+150's prime, 5 goroutines, channel buffer size is 2
+             user     system      total        real
+receive 12.303000   0.000000  12.303000 ( 12.304000)
+
+150's prime, 5 goroutines, channel buffer size is 3
+             user     system      total        real
+receive 11.299000   0.000000  11.299000 ( 11.299000)
